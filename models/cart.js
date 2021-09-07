e7932b3fd11b4b8df7b1ac1d7ee2d59f63fdf3a6
firecarl <blueskyfred@163.com>
2021-09-07 16:41:52 +0800

    为购物车添加添加和删除逻辑

--- models/cart.js
+++ models/cart.js
@@ -1,6 +1,10 @@
 class Cart {
   static SKU_MIN_COUNT = 1
   static SKU_MAX_COUNT = 77
+  static CART_ITEM_MAX_COUNT = 77
+  static STORAGE_KEY = 'key'
+
+  _cartData = null
 
   constructor() {
     if (typeof Cart.instance === 'object') {
@@ -8,6 +12,94 @@ class Cart {
     }
     Cart.instance = this
     return this
+  }
+
+  addItem(newItem) {
+    if (this.beyondMaxCartItemCount()) {
+      throw new Error('超过购物车最大数量')
+    }
+    this._pushItem(newItem)
+    this._refreshStorage()
+  }
+
+  removeItem(skuId) {
+    const oldItemIndex = this._findEqualItemIndex(skuId)
+    const cartData = this._getCartData()
+    cartData.items.splice(oldItemIndex, 1)
+    this._refreshStorage()
+  }
+
+  _findEqualItemIndex(skuId) {
+    const cartData = this._getCartData();
+    return cartData.items.findIndex(item => {
+      return item.skuId === skuId
+    })
+  }
+  _refreshStorage() {
+    wx.setStorageSync(STORAGE_KEY, this._cartData)
+  }
+
+  _pushItem(newItem) {
+    const cartData = this._getCartData()
+    const oldItem = this.findEqualItem(newItem.skuId)
+    if (!oldItem) {
+      cartData.items.unshift(newItem)
+    }else {
+      this._combineItem(oldItem, newItem)
+    }
+   
+  }
+
+  findEqualItem(skuId) {
+    let oldItem = null
+    const items = this._getCartData.items
+    for (let index = 0; index < items.length; index++) {
+      if (this._isEqualItem(items[i], skuId)) {
+        oldItem = items[index]
+        break
+      }
+    }
+    return oldItem
+  }
+
+  _isEqualItem(oldItem, skuId) {
+    return oldItem.skuId === skuId
+  }
+
+  _combineItem(oldItem, newItem) {
+    this._plusCount(oldItem, newItem.count)
+  }
+
+  _plusCount(item, count) {
+    item.count += count
+    if (item.count >= Cart.SKU_MAX_COUNT) {
+      item.count = Cart.SKU_MAX_COUNT
+    }
+  }
+
+  _getCartData() {
+    if (this._cartData != null) {
+      return this._cartData
+    }
+    let cartData = wx.getStorageSync(Cart.STORAGE_KEY)
+    if (!cartData) {
+      cartData = this._initCartDataStorage()
+    }
+    this._cartData = cartData
+    return cartData
+  }
+
+  _initCartDataStorage() {
+    const cartData = {
+      items:[]
+    }
+    wx.setStorageSync(STORAGE_KEY, cartData)
+    return cartData
+  }
+
+  beyondMaxCartItemCount() {
+    const cartData = this._getCartData()
+    return cartData.items.length >= Cart.CART_ITEM_MAX_COUNT;
   }
 }
 
